b3a827f6400b6f66f9ea014568bc7abdd59d99b4
Alexander <a.jegorov84@gmail.com>
2020-01-14 23:56:27 +0000

    initial commit

---
+++ src/Kata.java
@@ -0,0 +1,12 @@
+public class Kata {
+    public static int tvRemote(final String word) {
+        // start with col = 0; lastCol = 0; row = 0;
+        //find the index of the letter
+        //calculate row and column of the letter (row: index / 8; col: index % 8)
+        //add row to count
+        //add Math.abs(col - lastCol) to count
+        //update lastCol with col value
+        //repeat
+        return 1;
+    }
+}
---
+++ src/KataTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+
+public class KataTest {
+
+    @org.junit.Test
+    public void tvRemote() {
+        assertEquals(1, Kata.tvRemote("hello"));
+    }
+}
